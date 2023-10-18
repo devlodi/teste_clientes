a63c242c5604635e9ea22e4122be1bda7631911f
devlodi <143453595+devlodi@users.noreply.github.com>
2023-10-18 18:40:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
+// Importe a função createClient
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@latest/dist/umd/supabase.min.js';
+
 // Inicialize o Supabase
-const supabaseUrl = 'https://mlidfbitwixionbixmzv.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1saWRmYml0d2l4aW9uYml4bXp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc0NzgwODcsImV4cCI6MjAxMzA1NDA4N30.5lx39FmGKQpCI9euahCe_ljv-uCBYVqrbqccSlY-Oik';
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
-
-
-// Função para adicionar cliente
-async function addClient(nome, telefone) {
-  const { data, error } = await supabase
-    .from('clientes')
-    .insert([
-      { nome: nome, telefone: telefone }
-    ]);
-  
-  if (error) {
-    console.error('Erro ao adicionar cliente:', error);
-  } else {
-    console.log('Cliente adicionado com sucesso:', data);
-  }
+const supabase = createClient('https://mlidfbitwixionbixmzv.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1saWRmYml0d2l4aW9uYml4bXp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc0NzgwODcsImV4cCI6MjAxMzA1NDA4N30.5lx39FmGKQpCI9euahCe_ljv-uCBYVqrbqccSlY-Oik');
+// Função para adicionar um cliente ao Supabase
+async function adicionarCliente(nome, telefone) {
+    try {
+        const { data, error } = await supabase
+            .from('clientes')
+            .insert([{ nome: nome, telefone: telefone }]);
+        if (error) throw error;
+        alert('Cliente adicionado com sucesso!');
+    } catch (error) {
+        console.error('Erro:', error.message);
+    }
 }
 
-// Adicionar evento de submit ao formulário
-document.getElementById('add-client-form').addEventListener('submit', function(event) {
-  event.preventDefault();
-  
-  const nome = event.target.name.value;
-  const telefone = event.target.phone.value;
-
-  addClient(nome, telefone);
+// Adicione um ouvinte de evento ao formulário
+document.getElementById('cliente-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const nome = document.getElementById('nome').value;
+    const telefone = document.getElementById('telefone').value;
+    adicionarCliente(nome, telefone);
 });
